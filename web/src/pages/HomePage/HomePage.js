8a95425b4640bb3a1f01d0da91516ebc7e93d852
JLarky <jlarky@gmail.com>
2021-01-09 13:20:28 -0800

    show a video for 3 seconds

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,8 +1,67 @@
-export default () => (
-  <main>
-    <style
-      dangerouslySetInnerHTML={{
-        __html: `
+export default () => {
+  const [currentState, setPlayerState] = React.useState('UNSTARTED')
+  const playerRef = React.useRef(undefined)
+  const wrapperRef = React.useRef(null)
+  const [size, setSize] = React.useState({
+    width: '560',
+    height: '315',
+  })
+  const sizeRef = React.useRef(size)
+  sizeRef.current = size
+  React.useEffect(() => {
+    const { clientWidth: width, clientHeight: height } = wrapperRef.current
+    setSize({ width, height })
+  }, [])
+  React.useEffect(() => {
+    const tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    const firstScriptTag = document.getElementsByTagName('script')[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+    let player
+    var done = false
+
+    window.onYouTubeIframeAPIReady = () => {
+      console.log(
+        123,
+        size,
+        (playerRef.current = new YT.Player('yt-player', {
+          videoId: 'OOxR4YuXMmQ',
+          ...sizeRef.current,
+          events: {
+            onReady: (e) => {
+              console.log(222, e)
+              playerRef.current.playVideo()
+            },
+            onStateChange: (e) => {
+              setPlayerState(e.data)
+              const names = Object.keys(YT.PlayerState)
+              names.forEach((name) => {
+                if (e.data === YT.PlayerState[name]) {
+                  setPlayerState(name)
+                }
+              })
+            },
+          },
+        }))
+      )
+    }
+  }, [])
+
+  const done = React.useRef(false)
+  React.useEffect(() => {
+    if (currentState == 'PLAYING' && !done.current) {
+      setTimeout(() => {
+        playerRef.current.stopVideo()
+      }, 3000)
+      done.current = true
+    }
+  }, [currentState])
+
+  return (
+    <main>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
               html, body {
                 margin: 0;
               }
@@ -33,12 +92,17 @@ export default () => (
                 color: #2D3748;
               }
             `,
-      }}
-    />
-    <section>
-      <h1>
-        <span>Welcome to RedwoodJS!</span>
-      </h1>
-    </section>
-  </main>
-)
+        }}
+      />
+      <section>
+        <h1>
+          <span>Welcome to RedwoodJS!!!</span>
+          {currentState}
+          <div ref={wrapperRef}>
+            <div id="yt-player" height={300} />
+          </div>
+        </h1>
+      </section>
+    </main>
+  )
+}
